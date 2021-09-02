0c649ee8a9c19374efec12a3e29636ca1b9926ab
Amanjot Singh <theamanjs@gmail.com>
2021-09-02 23:56:56 +0530

    Add Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactGA from 'react-ga';
 
 function App() {
+  ReactGA.initialize('G-H7RL9FDDHD');
+  ReactGA.pageview(window.location.pathname);
+
+  const handleClick = () => {
+    console.log('click');
+    ReactGA.event({
+      category: 'User',
+      action: 'Create an Account'
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +29,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={handleClick}>Click here</button>
       </header>
     </div>
   );
